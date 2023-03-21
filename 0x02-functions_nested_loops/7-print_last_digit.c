124907cf6aecc1e81f8a3b397529276b56a7b894
Sanae Errami <erramisanae5@gmail.com>
2023-03-21 11:30:24 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @c: the number
+ * Return: the value of the last digit
+ */
+int print_last_digit(int c)
+{
+	int d;
+
+	d = c % 10;
+	return (d);
+}
