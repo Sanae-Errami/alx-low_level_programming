366cf41860c1e991d8485d4e90b572a9c3d52e4f
Sanae Errami <erramisanae5@gmail.com>
2023-03-21 05:39:08 -0700

    alpha 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 0;
 	char c;
 
-	for (i = 0; i < 10; i++)
+	while (i <= 9)
 	{
-		for (c = 'a'; i <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
